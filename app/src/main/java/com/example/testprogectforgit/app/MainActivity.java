4523c87c3c2d618b99a4a82a463c6dd6643334cf
Tracky <djerin_youriy@gmail.com>
2015-12-26 19:07:09 +0300

    init textview

--- app/src/main/java/com/example/testprogectforgit/app/MainActivity.java
+++ app/src/main/java/com/example/testprogectforgit/app/MainActivity.java
@@ -4,15 +4,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        textView= (TextView) findViewById(R.id.textView);    }
 
 
     @Override
